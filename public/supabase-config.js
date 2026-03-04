a9fb9bc9810dfdd82fab7d6910d4f111fc135c84
rantaiusang <baramultijavapalma@gmail.com>
2026-03-04 14:21:10 +0700

    Update and rename pi-config.js to supabase-config.js

---
+++ public/supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_CONFIG = {
+    url: "https://dikapquhusbwjccbqcsb.supabase.co", // Ganti dengan URL Supabase Anda
+    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpa2FwcXVodXNid2pjY2JxY3NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxNjQ1NTQsImV4cCI6MjA4NDc0MDU1NH0.X278oHDF0be7oa25484eliSukSYAYvDbJyU6ysz83zA" // Ganti dengan Anon Key Supabase Anda (Project Settings -> API)
+};
